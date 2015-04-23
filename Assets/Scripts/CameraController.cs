c46f9788bd227d18c43d23efe6fa9667919c265e
Jonathon Anderson <anderbubble@gmail.com>
2015-04-22 21:17:29 -0600

    Walls for the play area

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
 	public GameObject player;
 	private Vector3 offset;
 
 	// Use this for initialization
-	void Start () {
-		this.transform.LookAt(this.player.transform);
+	void Start ()
+	{
+		this.transform.LookAt (this.player.transform);
 		offset = this.transform.position - this.player.transform.position;
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
+	void LateUpdate ()
+	{
 		this.transform.position = this.player.transform.position + offset;
 	}
 }
