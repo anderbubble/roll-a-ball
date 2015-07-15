5f518e84b3d03ebdd44b1a27ee0b61698fbf7b66
Jonathon Anderson <anderbubble@gmail.com>
2015-07-14 19:58:03 -0600

    Upgrade unity and generate a pickup with a script

---
+++ Assets/Scripts/PickupManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupManager: MonoBehaviour {
+
+	public Transform pickupTemplate;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate(this.pickupTemplate, new Vector3(-5, 1, -5), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
